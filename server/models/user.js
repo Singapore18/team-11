8fa1b83beaf352499196d1ad8317606a79a8ea5b
Zhibin Yu <zhibinyu97@gmail.com>
2018-10-12 11:46:01 +0800

    New resume model that linked with user

--- server/models/user.js
+++ server/models/user.js
@@ -7,6 +7,10 @@ module.exports =
     mongoose.model('User', {
         // _id (with the undersocre) is to override the default id type to String, then maybe we can use email as id
         _id: String,
+        password: String,
+        salt: String,
         firstName: String,
-        lastName: String
+        lastName: String,
+        email: String,
+        resume: {type: Mongoose.Schema.Types.ObjectId, ref: 'Resume'}
     })
