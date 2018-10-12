f1682639a0fddc28f47318b6e675d4d27f97c063
Zhibin Yu <zhibinyu97@gmail.com>
2018-10-12 10:24:39 +0800

    Add comment to explain how to define mongodb model

--- server/models/user.js
+++ server/models/user.js
@@ -5,6 +5,7 @@ var mongoose = require('mongoose'),
 
 module.exports =
     mongoose.model('User', {
+        // _id (with the undersocre) is to override the default id type to String, then maybe we can use email as id
         _id: String,
         firstName: String,
         lastName: String
